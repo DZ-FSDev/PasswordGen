4788087a14a3ac791afc4288da19bcc4db2dfdae
DZ-FSDev <dz.uofm@gmail.com>
2022-06-20 14:17:32 -0500

    Parameterize pwlen default 12

--- box.js
+++ box.js
@@ -1,9 +1,8 @@
 var password = document.getElementById("password");
 
-function genPassword()
+function genPassword(passwordLength = 12)
 {
     let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let passwordLength = 12;
     let password = "";
 
     for(let i = 0; i <= passwordLength; i++)
@@ -11,6 +10,7 @@ function genPassword()
         let randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1);
     }
+    
     document.getElementById("password").value = password;
 }
 
