df2516c892f25e6a7271e1132b787effe5f55cf6
DZ-FSDev <dz.uofm@gmail.com>
2022-06-20 14:28:13 -0500

    Destructure charSets

--- box.js
+++ box.js
@@ -1,16 +1,21 @@
 var password = document.getElementById("password");
 
-function genPassword(passwordLength = 12)
+function genPassword(passwordLength = 12, strength = 'basic')
 {
-    let chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let charSet = {
+        basic: "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        weak: "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        moderate: "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        strong: "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    }
     let password = "";
 
     for(let i = 0; i <= passwordLength; i++)
     {
-        let randomNumber = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNumber, randomNumber + 1);
+        let randomNumber = Math.floor(Math.random() * charSet[strength].length);
+        password += charSet[strength].substring(randomNumber, randomNumber + 1);
     }
-    
+
     document.getElementById("password").value = password;
 }
 
